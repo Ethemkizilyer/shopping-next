907da4699c1fa7ba8a833cbee50274396b4c525f
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-14 02:05:56 +0300

    feat: updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,53 @@
 "use client";
-import NavBar from "../components/NavBar";
+import styled, { createGlobalStyle } from "styled-components";
+import { Provider } from "react-redux";
+import Head from "next/head";
 
+import store from "../store";
+import NavBar from "../components/NavBar";
+import ReactReduxFirebaseWrapper from "../components/ReactReduxFirebaseProvider";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    min-height: 100vh;
+    font-family: 'Inter', sans-serif;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  *::selection {
+    background-color: #b699f2;
+  }
+`;
+
+const Container = styled.div`
+  min-height: 100vh;
+  max-width: 1440px;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`;
 
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-        <NavBar />
-
-        {children}
+        <GlobalStyle />
+        <Container>
+          <Provider store={store}>
+            <ReactReduxFirebaseWrapper>
+              <NavBar />
+              {children}
+            </ReactReduxFirebaseWrapper>
+          </Provider>
+        </Container>
       </body>
     </html>
   );
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-   experimental: {
+  experimental: {
     appDir: true,
   },
   compiler: {
@@ -9,6 +9,9 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
   },
 };
 
