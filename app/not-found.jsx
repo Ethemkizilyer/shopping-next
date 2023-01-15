e1b24f16fd0b81cd99d9080817e552198d216585
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-15 06:11:24 +0300

    feat: app delete

--- app/not-found.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const notfound = () => {
-  return (
-    <div>ETHEM</div>
-  )
-}
-
-export default notfound;
--- components/BetterLink.jsx
+++ components/BetterLink.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const BetterLink = ({ href, children }) => {
   return (
